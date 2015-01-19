2be2128d0cdcc02539e94779fd51f6f85952abe8
dixxi <dixxi1@gmx.net>
2015-01-19 22:14:15 +0100

    added own httpclient with long enough timeout

--- src/Neo4j/Program.cs
+++ src/Neo4j/Program.cs
@@ -6,26 +6,27 @@ using System.Threading.Tasks;
 using Neo4jClient;
 using Neo4jClient.Cypher;
 using System.Diagnostics;
+using System.Net.Http;
 
 namespace Neo4j
 {
-    class Article
-    {
-        public string Name { get; set; }
-    }
+	class Article
+	{
+		public string Name { get; set; }
+	}
 
-    class Program
-    {
-        private static GraphClient client;
+	class Program
+	{
+		private static GraphClient client;
 
-        static void Main(string[] args)
-        {
+		static void Main(string[] args)
+		{
 			const string csvFile = "enwiki-20141208-pages-articles.titles.csv";
 
 			var sw = new Stopwatch();
 
 			try {
-				client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+				client = new GraphClient(new Uri("http://localhost:7474/db/data"), new HttpClientWrapper(new HttpClient() { Timeout = TimeSpan.FromDays(4) }));
 				client.Connect();
 
 				sw.Start();
@@ -63,29 +64,29 @@ CREATE (Page { id : line[0], title : line[1], ctitle : line[2], length : line[3]
 			{
 				Console.WriteLine("Error: {0}", exc.Message);
 			}
-            Console.ReadKey();
-        }
+			Console.ReadKey();
+		}
 
-        private static Article CreateArticle(string name)
-        {
-            var newArticle = new Article { Name = name };
-            
-            return client.Cypher
-                .Create("(article:Article {newArticle})")
-                .WithParam("newArticle", newArticle)
-                .Return<Article>("article")
-                .Results
-                .Single();
-        }
+		private static Article CreateArticle(string name)
+		{
+			var newArticle = new Article { Name = name };
+			
+			return client.Cypher
+				.Create("(article:Article {newArticle})")
+				.WithParam("newArticle", newArticle)
+				.Return<Article>("article")
+				.Results
+				.Single();
+		}
 
-        private static void LinkTo(Article source, Article target)
-        {
-            client.Cypher
-                .Match("(s:Article)", "(t:Article)")
-                .Where((Article s) => s.Name == source.Name)
-                .AndWhere((Article t) => t.Name == target.Name)
-                .CreateUnique("s-[:links_to]->t")
-                .ExecuteWithoutResults();
-        }
-    }
+		private static void LinkTo(Article source, Article target)
+		{
+			client.Cypher
+				.Match("(s:Article)", "(t:Article)")
+				.Where((Article s) => s.Name == source.Name)
+				.AndWhere((Article t) => t.Name == target.Name)
+				.CreateUnique("s-[:links_to]->t")
+				.ExecuteWithoutResults();
+		}
+	}
 }
