41854521eb201934a443cb7526f8e513a32490a0
dixxi <dixxi1@gmx.net>
2015-01-29 22:31:31 +0100

    merged new benchmarks

--- doc/queries.sql
+++ doc/queries.sql
@@ -1,48 +1,91 @@
-SELECT DISTINCT *
+SELECT COUNT(DISTINCT(l.src))
 FROM (
-SELECT a.*
-FROM Page p
-  JOIN Link l ON p.id=l.dst
-  JOIN Page a ON a.id=l.src
-WHERE p.title='Philosophie'
- UNION ALL
-SELECT a.*
-FROM Page p
-  JOIN Link l ON p.id=l.dst
-  JOIN Link h ON h.dst=l.src
-  JOIN Page a ON a.id=h.src
-WHERE p.title='Philosophie'
- UNION ALL
-SELECT a.*
-FROM Page p
-  JOIN Link l ON p.id=l.dst
-  JOIN Link h1 ON h1.dst=l.src
-  JOIN Link h2 ON h2.dst=h1.src
-  JOIN Page a ON a.id=h2.src
-WHERE p.title='Philosophie'
- UNION ALL
-SELECT a.*
-FROM Page p
-  JOIN Link l ON p.id=l.dst
-  JOIN Link h1 ON h1.dst=l.src
-  JOIN Link h2 ON h2.dst=h1.src
-  JOIN Link h3 ON h3.dst=h2.src
-  JOIN Page a ON a.id=h3.src
-WHERE p.title='Philosophie'
-) a;
+		SELECT l1.src
+		FROM Page p
+			JOIN Link l1 ON p.id = l1.dst
+		WHERE p.title='Jesus'
+	UNION ALL
+		SELECT l2.src
+		FROM Page p
+			JOIN Link l1 ON p.id = l1.dst
+			JOIN Link l2 ON l2.dst = l1.src
+		WHERE p.title='Jesus'
+	UNION ALL
+		SELECT l3.src
+		FROM Page p
+			JOIN Link l1 ON p.id = l1.dst
+			JOIN Link l2 ON l2.dst = l1.src
+			JOIN Link l3 ON l3.dst = l2.src
+		WHERE p.title='Jesus'
+	UNION ALL
+		SELECT l4.src
+		FROM Page p
+			JOIN Link l1 ON p.id = l1.dst
+			JOIN Link l2 ON l2.dst = l1.src
+			JOIN Link l3 ON l3.dst = l2.src
+			JOIN Link l4 ON l4.dst = l3.src
+		WHERE p.title='Jesus'
+	UNION ALL
+		SELECT l5.src
+		FROM Page p
+			JOIN Link l1 ON p.id = l1.dst
+			JOIN Link l2 ON l2.dst = l1.src
+			JOIN Link l3 ON l3.dst = l2.src
+			JOIN Link l4 ON l4.dst = l3.src
+			JOIN Link l5 ON l5.dst = l4.src
+		WHERE p.title='Jesus'
+) l;
 
-
-
+SELECT *
+FROM Page p
+	JOIN (
+		SELECT DISTINCT(l.src)
+		FROM (
+				SELECT l1.src
+				FROM Page p
+					JOIN Link l1 ON p.id = l1.dst
+				WHERE p.title='Jesus'
+			UNION ALL
+				SELECT l2.src
+				FROM Page p
+					JOIN Link l1 ON p.id = l1.dst
+					JOIN Link l2 ON l2.dst = l1.src
+				WHERE p.title='Jesus'
+			UNION ALL
+				SELECT l3.src
+				FROM Page p
+					JOIN Link l1 ON p.id = l1.dst
+					JOIN Link l2 ON l2.dst = l1.src
+					JOIN Link l3 ON l3.dst = l2.src
+				WHERE p.title='Jesus'
+			UNION ALL
+				SELECT l4.src
+				FROM Page p
+					JOIN Link l1 ON p.id = l1.dst
+					JOIN Link l2 ON l2.dst = l1.src
+					JOIN Link l3 ON l3.dst = l2.src
+					JOIN Link l4 ON l4.dst = l3.src
+				WHERE p.title='Jesus'
+			UNION ALL
+				SELECT l5.src
+				FROM Page p
+					JOIN Link l1 ON p.id = l1.dst
+					JOIN Link l2 ON l2.dst = l1.src
+					JOIN Link l3 ON l3.dst = l2.src
+					JOIN Link l4 ON l4.dst = l3.src
+					JOIN Link l5 ON l5.dst = l4.src
+				WHERE p.title='Jesus'
+		) l
+	) l ON p.id = l.src;
 
 WITH temp (id) AS (
-  SELECT p.id
-  FROM Page p
-  WHERE p.title='Philosophie'
- UNION ALL
-  SELECT fl.src
-  FROM FirstLink fl
-    JOIN temp t ON fl.dst=t.id
+		SELECT p.id
+		FROM Page p
+		WHERE p.title='Philosophie'
+	UNION ALL
+		SELECT fl.src
+		FROM FirstLink fl
+			JOIN temp t ON fl.dst=t.id
 )
-SELECT COUNT(DISTINCT(a.id))
-FROM Page a
-  JOIN temp t ON a.id=t.id;
+SELECT COUNT(DISTINCT(t.id)) - 1
+FROM temp t;
